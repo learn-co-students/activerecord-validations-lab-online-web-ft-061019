54c1dbdbae5e4b453c004d857d67b23296874909
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-08-21 16:27:36 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, clickbait_title: true
+    validates :content, length: {minimum: 250}, presence: true
+    validates :summary, length: {maximum: 250}, presence: true
+    validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
+
 end
